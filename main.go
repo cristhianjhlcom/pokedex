47295375e577dca34ff6fea32226d513949cacd4
cristhianjhl.com <cristhianjhl.web@gmail.com>
2024-09-20 13:17:09 -0500

    add explore callback to fetch all the areas on certain location

--- main.go
+++ main.go
@@ -31,13 +31,17 @@ func main() {
 			continue
 		}
 		commandName := cleaned[0]
+		args := []string{}
+		if len(cleaned) > 1 {
+			args = cleaned[1:]
+		}
 		availableCommands := getCommands()
 		command, ok := availableCommands[commandName]
 		if !ok {
 			fmt.Println("invalid command")
 			continue
 		}
-		err := command.callback(&config)
+		err := command.callback(&config, args...)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -53,7 +57,7 @@ type Config struct {
 type CLICommand struct {
 	name        string
 	description string
-	callback    func(*Config) error
+	callback    func(*Config, ...string) error
 }
 
 type Client struct {
@@ -69,6 +73,33 @@ type LocationAreaResponse struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"results"`
+}
+
+type LocationArea struct {
+	Areas []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"areas"`
+	GameIndices []struct {
+		GameIndex  int `json:"game_index"`
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+	} `json:"game_indices"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	Region struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"region"`
 }
 
 func NewClient(cacheInterval time.Duration) Client {
@@ -121,6 +152,44 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreaResponse, error
 	return locationAreasResponse, nil
 }
 
+func (c *Client) GetLocationArea(locationAreaName string) (LocationArea, error) {
+	endpoint := "/location/" + locationAreaName
+	fullURL := baseURL + endpoint
+	data, ok := c.cache.Get(fullURL)
+	if ok {
+		// cache hit.
+		locationArea := LocationArea{}
+		err := json.Unmarshal(data, &locationArea)
+		if err != nil {
+			return LocationArea{}, err
+		}
+		return locationArea, nil
+	}
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode > 399 {
+		return LocationArea{}, fmt.Errorf("bad status code: %v", response.StatusCode)
+	}
+	data, err = io.ReadAll(response.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	locationArea := LocationArea{}
+	err = json.Unmarshal(data, &locationArea)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	c.cache.Add(fullURL, data)
+	return locationArea, nil
+}
+
 func getCommands() map[string]CLICommand {
 	return map[string]CLICommand{
 		"help": {
@@ -138,6 +207,11 @@ func getCommands() map[string]CLICommand {
 			description: "Lists the previous page of locations areas",
 			callback:    callbackMapb,
 		},
+		"explore": {
+			name:        "explore {location_area}",
+			description: "List the areas in location",
+			callback:    callbackExplorer,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Turns off the pokedex",
@@ -146,7 +220,7 @@ func getCommands() map[string]CLICommand {
 	}
 }
 
-func callbackHelp(config *Config) error {
+func callbackHelp(config *Config, args ...string) error {
 	fmt.Println("Welcome to the Pokedex help menu!")
 	fmt.Println("Here are you available commands: ")
 	availableCommands := getCommands()
@@ -157,12 +231,29 @@ func callbackHelp(config *Config) error {
 	return nil
 }
 
-func callbackExit(config *Config) error {
+func callbackExit(config *Config, args ...string) error {
 	os.Exit(0)
 	return nil
 }
 
-func callbackMap(config *Config) error {
+func callbackExplorer(config *Config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("No location area provided")
+	}
+	locationArea := args[0]
+	response, err := config.pokeAPIClient.GetLocationArea(locationArea)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	fmt.Printf("Areas in %s \n", locationArea)
+	for _, area := range response.Areas {
+		fmt.Printf(" - %s\n", area.Name)
+	}
+	return nil
+}
+
+func callbackMap(config *Config, args ...string) error {
 	response, err := config.pokeAPIClient.ListLocationAreas(config.nextLocationAreaURL)
 	if err != nil {
 		log.Fatal(err)
@@ -177,7 +268,7 @@ func callbackMap(config *Config) error {
 	return nil
 }
 
-func callbackMapb(config *Config) error {
+func callbackMapb(config *Config, args ...string) error {
 	if config.previousLocationAreaURL == nil {
 		return errors.New("You are on the first page")
 	}
