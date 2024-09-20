5c9a9a41fce551deef2b468ce601a8ac2bf0ce6c
cristhianjhl.com <cristhianjhl.web@gmail.com>
2024-09-19 22:51:05 -0500

    make pokedex repl

--- main.go
+++ main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Pokedex app")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		commandName := cleaned[0]
+		availableCommands := getCommands()
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("invalid command")
+			continue
+		}
+		command.callback()
+	}
+}
+
+type CLICommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]CLICommand {
+	return map[string]CLICommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Turns off the pokedex",
+			callback:    callbackExit,
+		},
+	}
+}
+
+func callbackHelp() error {
+	fmt.Println("Welcome to the Pokedex help menu!")
+	fmt.Println("Here are you available commands: ")
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf(" - %s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println("")
+	return nil
+}
+
+func callbackExit() error {
+	os.Exit(0)
+	return nil
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
 }
